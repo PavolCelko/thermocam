d76d1e350df505e4f0d7daee645da6d88c7730a0
palino <palino.celko@gmail.com>
2019-02-19 06:42:50 +0100

    data read but on Calc To eroor: division by zero

--- run.py
+++ run.py
@@ -12,41 +12,59 @@ def print_reg(reg):
     print("PASS...")
 
 def main():
-    print("Firstly, NOTHING.")
     
     sensor = mlx_api.Mlx()
 
-    mode = sensor.MLX90640_GetCurMode()
-    print(hex(mode))
-    sensor.MLX90640_SetChessMode()
-    mode = sensor.MLX90640_GetCurMode()
-    print(hex(mode))
-    sensor.MLX90640_SetInterleavedMode()
-    mode = sensor.MLX90640_GetCurMode()
-    print(hex(mode))
+    # mode = sensor.MLX90640_GetCurMode()
+    # print(hex(mode))
+    # sensor.MLX90640_SetChessMode()
+    # mode = sensor.MLX90640_GetCurMode()
+    # print(hex(mode))
+    # sensor.MLX90640_SetInterleavedMode()
+    # mode = sensor.MLX90640_GetCurMode()
+    # print(hex(mode))
 
     ee_data = sensor.MLX90640_DumpEE()
     print("dump EE DATA len: ")
     print(len(ee_data))
-
-    print("data 51: ")
-    print(ee_data[51])
+    print("EE data: ")
+    print(ee_data)
 
     frame_list = sensor.MLX90640_GetFrameData()
     print("FRAME len: ")
     print(len(frame_list))
+    print("frame : ")
+    print(frame_list)
 
-    print("frame 810: ")
-    print(frame_list[810])
+    Ta = sensor.MLX90640_GetTa(frame_list)
+    print("Ta: ")
+    print(Ta)
 
-    res = sensor.MLX90640_GetVdd(frame_list)
-    print("res: ")
-    print(res)
+    # frame_list = sensor.MLX90640_CalculateTo()
+    # print("FRAME len: ")
+    # print(len(frame_list))
+    # print("frame 810: ")
+    # print(frame_list[810])
+
+    vdd = sensor.MLX90640_GetVdd(frame_list)
+    print("vdd: ")
+    print(vdd)
+
+    image = sensor.MLX90640_CalculateTo(frame_list, 1, Ta)
+    print("calc To len: ")
+    print(len(image))
+    print("calc To IMAGE: ")
+    print(image)
+
+    image = sensor.MLX90640_GetImage(frame_list)
+    print("IMAGE len: ")
+    print(len(image))
+    print("IMAGE: ")
+    print(image)
+
+    # print(frame_list)
+
     
-    # print("unknown mode {:X}".format(mode))
-
-    fun2()
-
 if __name__ == '__main__':
     main()
         
