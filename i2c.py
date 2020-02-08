f87cda8ab9e61d0cd3f72415d66d597cdb8f3247
palko <palino.celko@gmail.com>
2020-02-08 12:03:07 +0000

    added write protection of EEPROM and Reserved Registers

--- i2c.py
+++ i2c.py
@@ -4,6 +4,15 @@ from smbus2 import SMBus, i2c_msg, SMBusWrapper
 
 TWO_BYTES_LEN = 2
 
+class EEWriteError(Exception):
+    """!!!attepmt to write to EEPROM memory which is 10 times only writable."""
+    pass
+
+class ReservedRegWrite(Exception):
+    """!!!attepmt to write to Reserved Reg memory."""
+    pass
+
+
 class MlxI2C:
     def __init__(self, slave_addr):
         self.slave_addr = slave_addr
@@ -17,6 +26,20 @@ class MlxI2C:
 
     #int MLX90640_I2CWrite(uint8_t slaveAddr,uint16_t writeAddress, uint16_t data)
     def MLX90640_I2CWriteReg(self, writeAddress, data):
+        
+        if 0x2400 <= writeAddress and writeAddress <= 0x273F:
+            raise EEWriteError(Exception)
+
+        if 0x8000 <= writeAddress and writeAddress <= 0x800C:
+            # this is the only valid option for writing to the 0x8000 status register defined by MLX driver
+            if 0x8000 == writeAddress and data == 0x0030:
+                pass            
+            else:
+                raise ReservedRegWrite(Exception)
+        
+        if 0x8011 <= writeAddress and writeAddress <= 0x8016:
+            raise ReservedRegWrite(Exception)
+
         reg_address = []
         reg_address.append((writeAddress & 0xFF00) >> 8)
         reg_address.append(writeAddress & 0x00FF)
